325bbd4ea79bc9442eccd9a79603fae626e3b1bb
Shah Ali Hridoy <shahalihridoy@gmail.com>
2022-12-03 21:37:45 +0600

    Fix null error

--- scripts/content.js
+++ scripts/content.js
@@ -2,18 +2,20 @@
 const newsFeed = document.getElementById("ssrb_feed_start");
 const postContainer = newsFeed?.nextElementSibling?.childNodes[1];
 
-// add mutation observer to the post container
-const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    const posts = mutation.addedNodes || [];
-    posts.forEach((post) => {
-      if (post.innerText.includes("Reels and short videos")) {
-        post.style.display = "none";
-      }
+if (postContainer) {
+  // add mutation observer to the post container
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      const posts = mutation.addedNodes || [];
+      posts.forEach((post) => {
+        if (post.innerText.includes("Reels and short videos")) {
+          post.style.display = "none";
+        }
+      });
     });
   });
-});
 
-observer.observe(postContainer, {
-  childList: true,
-});
+  observer.observe(postContainer, {
+    childList: true,
+  });
+}
